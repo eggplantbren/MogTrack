777bf3477dd5e16a6de03489feeae9e08be1c4a4
Brendon Brewer <brendon.brewer@gmail.com>
2015-10-26 06:15:30 +1300

    Gutted in preparation for going to periodic GP

--- code/julia/Track.jl
+++ code/julia/Track.jl
@@ -10,75 +10,26 @@ type Track
 	# Number of points in the trajectory
 	num_points::Int64
 
+	# Hyperparameters
+	amplitude::Float64
+	period::Float64
+	length_scale::Float64
+
 	# The trajectory itself
-	y::Array{Float64, 2}
+	y::Array{Float64, 1}
 end
 
 @doc """
 Constructor. Input: num_dimensions, num_points
 """ ->
 function Track(num_dimensions::Int64, num_points::Int64)
-	return Track(num_dimensions, num_points,
-						Array(Float64, (num_points, num_dimensions)))
+	return Track(num_dimensions, num_points, 1.0, 1.0, 0.1,
+						Array(Float64, (num_points, )))
 end
 
-@doc """
-Set initial conditions for the track
-""" ->
-function initialise!(track::Track)
-	for(j in 1:track.num_dimensions)
-		for(i in 1:track.num_points)
-			track.y[i, j] = 0.0
-		end
-	end
-	return nothing
-end
+# Old idea: use numerical solver for stochastic PDE
+# with periodic BCs as a proposal.
+# New idea: use periodic GP kernel.
 
-
-@doc """
-Update (solve the diffusion equation driven by white noise with periodic
-boundary conditions)
-""" ->
-function update!(track::Track; dt::Float64=0.01)
-	# Second spatial derivative
-	d2 = Array(Float64, size(track.y))
-	for(j in 1:size(d2)[2])
-		for(i in 2:size(d2)[1]-1)
-			d2[i, j] = track.y[i+1, j] + track.y[i-1, j] - 2*track.y[i, j]
-		end
-		# Boundary points
-		d2[1, j] = track.y[2, j] + track.y[end, j] - 2*track.y[1, j]
-		d2[end, j] = track.y[1, j] + track.y[end-1, j] - 2*track.y[end, j]
-	end
-
-	# Do the update
-	for(j in 1:size(d2)[2])
-		for(i in 1:size(d2)[1])
-			track.y[i, j] += dt*d2[i, j] + 0.0001*randn()
-		end
-	end
-	return nothing
-end
-
-
-
-
-using PyCall
-@pyimport matplotlib.pyplot as plt
-
-# Initialise with a spike
 track = Track(1, 100)
-initialise!(track)
-
-# Solve diffusion equation
-plt.ion()
-for(i in 1:1000)
-	update!(track, dt=0.05)
-	plt.hold(false)
-	plt.plot(track.y)
-	plt.draw()
-end
-
-plt.ioff()
-plt.show()
 
